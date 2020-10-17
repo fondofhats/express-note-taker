1e454f978ec7bd59ce0a50b7da61ee8db63d695a
Michael Giddings <mikegiddings@gmail.com>
2020-10-17 19:30:14 -0400

    added files and folders for intial setup

---
+++ server.js
@@ -0,0 +1,7 @@
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
+
+const express = require('express');
+const PORT = process.env.PORT || 3001;
+
+
